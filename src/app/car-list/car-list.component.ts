2f4716e9cd0c0bd8617791f1f227e9def7626040
Helkindown <Helkindown@protonmail.com>
2022-03-11 23:23:52 +0100

    REFACTOR: car-list component

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
-interface CarDataInterface {
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.css']
+})
+export class CarListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    loadCars(new URL("http://localhost:8080/api/v1/vehicles"));
+  }
+}
+
+interface CarInterface {
   id: number;
   licencePlate: string;
   brand: string;
@@ -12,67 +26,53 @@ interface CarDataInterface {
   urlImg: string;
 }
 
-const xhr = new XMLHttpRequest();
+/** Return table of all cars if success; otherwise it will return undefined and call alert */
+function loadCars(theURL: URL): void {
+    const xhr = new XMLHttpRequest();
 
-// 2. Configure it: GET-request for the URL /article/.../load
-xhr.open('GET', 'http://localhost:8080/api/v1/vehicles');
+    xhr.open("GET", theURL, true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status != 200) {
+            alert(`Error ${xhr.status}: ${xhr.statusText}\nIs the backend server is running locally?`);
+            return;
+        }
+        const cars = JSON.parse(xhr.response) as CarInterface[];
 
-// 3. Send the request over the network
-xhr.send();
+        const table = document.createElement("table");
+        table.tHead = createTableHeader();
+        populateTableBody(table.createTBody(), cars);
+        displayTable(table);
+    };
+}
 
-// 4. This will be called after the response is received
-xhr.onload = function() {
-  if (xhr.status != 200) { // analyze HTTP status of the response
-    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-    return;
-  }
-  const data = JSON.parse(xhr.response) as CarDataInterface[];
+/** Create a <thead> dom element describing car table content. Pure function  */
+function createTableHeader(): HTMLTableSectionElement {
+    const head = document.createElement("thead");
+    const row = head.insertRow();
+    ["Licence Plate", "Brand", "Model", "Color", "Price", "Price per Km", "Horse Power", "Image"].map(x => row.insertCell().innerText = x);
+    return head;
+}
 
-  const responseHTML = document.getElementById("response") as HTMLTableElement;
-  for(let car of data) {
-    const row = responseHTML.insertRow();
-      row.insertCell().innerText = String(car.id);
-      row.insertCell().innerText = car.licencePlate;
-      row.insertCell().innerText = car.brand;
-      row.insertCell().innerText = car.model;
-      row.insertCell().innerText = car.color;
-      row.insertCell().innerText = String(car.rentalPrice);
-      row.insertCell().innerText = String(car.kmPrice);
-      row.insertCell().innerText = String(car.horsePower);
+/** Self describing name, fille the give tbody with cars.
+ *  I really wanted to create a pure function, but no methods exist in DOM to do something like table.tBody = pureFunction so curses  */
+function populateTableBody(tbody: HTMLTableSectionElement, cars: CarInterface[]): void {
+    for (let car of cars) {
+        const row = tbody.insertRow();
+        row.insertCell().innerText = car.licencePlate;
+        row.insertCell().innerText = car.brand;
+        row.insertCell().innerText = car.model;
+        row.insertCell().innerText = car.color;
+        row.insertCell().innerText = (car.rentalPrice * 1000) + " €";
+        row.insertCell().innerText = String(car.kmPrice);
+        row.insertCell().innerText = String(car.horsePower);
+        const img = document.createElement("img");
+        img.alt = "Picture of the car " + car.licencePlate;
+        img.src = car.urlImg;
+        row.insertCell().appendChild(img);
+    }
+}
 
-      const img = document.createElement("img") as HTMLImageElement;
-
-      img.alt = "Image de la voiture " + car.licencePlate;
-      img.src = car.urlImg;
-
-      row.insertCell().appendChild(img);
-  }
-};
-
-xhr.onprogress = function(event) {
-  if (event.lengthComputable) {
-    console.log(`Received ${event.loaded} of ${event.total} bytes`);
-  } else {
-    console.log(`Received ${event.loaded} bytes`); // no Content-Length
-  }
-
-};
-
-xhr.onerror = function() {
-  console.log("Request failed");
-};
-
-
-@Component({
-  selector: 'app-car-list',
-  templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.css']
-})
-export class CarListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+function displayTable(table: HTMLTableElement): void {
+    document.getElementById("car-list-main")?.appendChild(table);
 }

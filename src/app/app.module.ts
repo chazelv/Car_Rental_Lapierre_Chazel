44cdd6f9ac0daa6791e7d61f2e25a2e12e5cc28a
Alexis Lapierre <alexis.lapierre@etu.univ-grenoble-alpes.fr>
2022-03-18 16:00:21 +0100

    Create component car-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarListComponent } from './cars/car-list/car-list.component';
 import { NavbarComponent } from './Homepage/navbar/navbar/navbar.component';
+import { CarDetailsComponent } from './cars/car-details/car-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarListComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarDetailsComponent
   ],
   imports: [
     BrowserModule,

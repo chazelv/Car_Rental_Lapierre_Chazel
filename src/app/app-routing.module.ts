276ba60234ce8ce3896f1951b26c1f3c950a9172
Alexis Lapierre <alexis.lapierre@etu.univ-grenoble-alpes.fr>
2022-03-18 15:59:17 +0100

    Move car-list component to subfolder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CarListComponent } from './car-list/car-list.component';
+import { CarListComponent } from './cars/car-list/car-list.component';
 
 const routes: Routes = [
   { path: "car-list", component: CarListComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarListComponent } from './car-list/car-list.component';
+import { CarListComponent } from './cars/car-list/car-list.component';
 import { NavbarComponent } from './Homepage/navbar/navbar/navbar.component';
 
 @NgModule({

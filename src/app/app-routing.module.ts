eb9a1705e34248d67f0fa2d91e7e057d0bfec01a
Alexis Lapierre <alexis.lapierre@etu.univ-grenoble-alpes.fr>
2022-03-18 16:03:20 +0100

    Add car-details to nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarDetailsComponent } from './cars/car-details/car-details.component';
 import { CarListComponent } from './cars/car-list/car-list.component';
 
 const routes: Routes = [
-  { path: "car-list", component: CarListComponent}
+  { path: "car-list", component: CarListComponent},
+  { path: "car-details", component: CarDetailsComponent}
 ];
 
 @NgModule({

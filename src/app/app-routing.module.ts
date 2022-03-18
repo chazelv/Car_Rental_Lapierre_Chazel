fdce907d1990a25b90e45138fb1cb66510637f7d
Val <Valentin.Chazel@etu.univ-grenoble-alpes.fr>
2022-03-18 15:41:31 +0100

    Create the Connexion page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
+import { ConnexionPageComponent } from './connexion-page/connexion-page.component';
 
 const routes: Routes = [
-  { path: "car-list", component: CarListComponent}
+  { path: "car-list", component: CarListComponent},
+  { path: "connexion-page", component: ConnexionPageComponent }
 ];
 
 @NgModule({

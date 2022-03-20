e15f8ef54b65b3f2eb63074e05a08c8da669ceb7
Helkindown <Helkindown@protonmail.com>
2022-03-20 01:16:02 +0100

    First step for car-details component

--- src/app/cars/car-details/car-details.component.ts
+++ src/app/cars/car-details/car-details.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-details.component.css']
 })
 export class CarDetailsComponent implements OnInit {
+    // can't be put as private bc it's used by searchLicensePlate outside of the class
+    #input!: HTMLInputElement;
+    #output!: HTMLOutputElement;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.#input = document.getElementById("license-plate-input") as HTMLInputElement;
+        this.#output = document.getElementById("license-plate-output") as HTMLOutputElement;
 
+        const form = document.getElementById("license-plate-form") as HTMLFormElement;
+        form.oninput = this.searchLicensePlateTemplate();
+    }
+
+    /** Stupid workaround to have reference to this */
+    searchLicensePlateTemplate(): () => void {
+        return () => {
+            this.#output.innerText = this.#input.value;
+        }
+    }
 }

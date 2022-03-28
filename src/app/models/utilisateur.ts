bb30f8f2d2c7c3ff4fb33f0069d2e693dfbf7638
Val <Valentin.Chazel@etu.univ-grenoble-alpes.fr>
2022-03-28 11:23:34 +0200

    feat : add the model utilisateur

---
+++ src/app/models/utilisateur.ts
@@ -0,0 +1,16 @@
+export class Utilisateur{
+    public address!: string;
+    public admin!: boolean;
+    public birth!: Date;
+    public driverLicenceNumber!: string;
+    public drivingLicenceExpiration!: Date;
+    public drivingLicenceGetting!: Date;
+    public firstName!: string;
+    public id!: number ;
+    public login!: string;
+    public name!: string;
+    public pwd!: string;
+    public token!: string;
+    public vehicleClass!: string;
+}
+
